3d9f1c8d9afc4224f6a2a54dc209201388b774c8
beck <beck@eventbrite.com>
2015-11-23 13:43:11 -0600

    fix mobile menu

--- js/menu.js
+++ js/menu.js
@@ -1,30 +1,64 @@
 ;(function(){
     var hamburger = $('.hamburger'),
-    hamburgerX = $('.hamburgerx'),
-    menu = $('.header-menu'),
-    config = {
-        'hamburger': 'url("../images/burger.png")',
-        'hamburgerX': 'url("../images/burgerX.png")'
-    };
+        hamburgerX = $('.hamburgerx'),
+        menu = $('.header-menu'),
+        menuBlock = $('.mobile-menu-block'),
+        menuLinks = $('.header-menu li'),
+        mq = window.matchMedia("(max-width: 850px)");
+        browserHeight = $(window).height() + getExtraPixels();
+        config = {
+            'hamburger': 'url("../images/burger.png")',
+            'hamburgerX': 'url("../images/burgerX.png")',
+        };
 
-  hamburger.click(function() {
-    OpenMenu();
-  });
 
-  hamburgerX.click(function () {
-    closeMenu();
-  });
+    $(document).ready(function(){
+        headerHeightMobile();
+    });
 
-  function OpenMenu() {
-    hamburger.fadeOut();
-    hamburgerX.fadeIn('fast');
-    menu.addClass('menu-open');
-  }
+    hamburger.click(function() {
+        OpenMenu();
+    });
 
-  function closeMenu() {
-    hamburgerX.fadeOut();
-    hamburger.fadeIn('fast');
-    menu.removeClass('menu-open');
-  }
+    hamburgerX.click(function () {
+        closeMenu();
+    });
 
+    menuLinks.each(function(link){
+        $(this).click(function(){
+            closeMenu();
+        });
+    });
+
+    $(window).resize(function(){
+        headerHeightMobile();
+    });
+
+    function OpenMenu() {
+        hamburger.fadeOut();
+        hamburgerX.fadeIn('fast');
+        menu.addClass('menu-open');
+        menuBlock.addClass('mobile-block-show');
+    }
+
+    function closeMenu() {
+        hamburgerX.fadeOut();
+        hamburger.fadeIn('fast');
+        menu.removeClass('menu-open');
+        menuBlock.removeClass('mobile-block-show');
+    }
+
+    function getExtraPixels() {
+        if(navigator.userAgent.indexOf('iPhone') != -1) {
+          return 60;
+        }
+
+        return 0;
+    }
+
+    function headerHeightMobile(){
+        if(mq.matches) {
+            menu.css('height', browserHeight);
+        }
+    }
 })();
