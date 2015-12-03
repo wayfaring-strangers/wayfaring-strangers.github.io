949b8df2944d4b79204f8e174f4f93136aa04774
beck <beck410@hotmail.com>
2015-12-02 18:57:28 -0600

    fixed orientation image dimension problem

--- js/imageDimensions.js
+++ js/imageDimensions.js
@@ -1,16 +1,14 @@
 ;(function(){
-  $(document).ready(function(){
-    setImageDimensions();
-  });
+    $(document).ready(function(){
+        setImageDimensions();
+    });
 
-  $(window).resize(function(){
-    setImageDimensions();
-  });
+    $(window).resize(function(){
+        console.log('resize');
+        setImageDimensions();
+    });
 
     function setImageDimensions(){
-        // var mq = window.matchMedia("(min-width: 500px)");
-        // if mq.matches
-
         var extraPixels = 0;
 
         if(navigator.userAgent.indexOf('iPhone') != -1) {
@@ -22,9 +20,9 @@
 
         $('.page').each(function() {
             if($(this).height() < browserHeight) {
-                $(this).css('width', browserWidth);
                 $(this).css('height', browserHeight);
             }
+            $(this).css('width', browserWidth);
         });
     }
 })();
